6efc5741cd5fb7c2f5950cd550c6ef46f0312d9b
hugovictoor <silvestrinivictor@gmail.com>
2023-06-28 19:25:42 -0300

    Adding useState and working on them with forms

--- 01-starting-setup/src/App.jsx
+++ 01-starting-setup/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Expenses from "./components/Expenses.js"
+import NewExpense from "./components/NewExpense/NewExpense.js";
 
 function App() {
   const expenses = [
@@ -25,7 +26,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,25 @@
-import React from "react";
-import Card from "./Card"
+import React, { useState } from "react";
+import Card from "./Card";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
+const [title, setTitle] = useState(props.expense.title);
+  
+
+  const clickHandler = () => {
+    setTitle("Updated!")
+    console.log("Clicked!!!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.expense.date} />
       <div className="expense-item__description">
-        <h2>{props.expense.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.expense.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
