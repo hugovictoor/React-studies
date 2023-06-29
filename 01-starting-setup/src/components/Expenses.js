dacb16604e1af359495f74ff8bddf4fdb2980b82
hugovictoor <silvestrinivictor@gmail.com>
2023-06-29 16:22:43 -0300

    Working on derived states and cleaning some code (:

--- 01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/src/components/Expenses.js
@@ -1,11 +1,32 @@
 import ExpenseItem from "./ExpenseItem";
-import React from "react";
-import "./Expenses.css"
-import Card from "./Card"
+import React, { useState } from "react";
+import "./Expenses.css";
+import Card from "./Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+  const [year, setYear] = useState("2021");
+
+  // let infoText = "2019, 2020 & 2021";
+
+  // if (year === "2019") {
+  //   infoText = "2020, 2021, 2022";
+  // } else if (year === "2020") {
+  //   infoText = "2019, 2021, 2022";
+  // } else if (year === "2021") {
+  //   infoText = "2019, 2020, 2022";
+  // } else if (year === "2022") {
+  //   infoText = "2019, 2020, 2021";
+  // }
+
+  const yearChoiceHandler = (enteredYear) => {
+    setYear(enteredYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={year} onYearChoice={yearChoiceHandler} />
+      {/* <p>Data from {infoText} is Hidden</p> */}
       <ExpenseItem expense={props.items[0]} />
       <ExpenseItem expense={props.items[1]} />
       <ExpenseItem expense={props.items[2]} />
